2b68c350b985a25838d61fcc046e5192997b856a
microgoz <andres.gonzalez2392@gmail.com>
2017-12-13 17:14:47 -0500

    Initial Commit

---
+++ SuperMarketApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SuperMarketApp
+//
+//  Created by Andres Gonzalez on 12/13/17.
+//  Copyright © 2017 Andres Gonzalez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ SuperMarketApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SuperMarketApp
+//
+//  Created by Andres Gonzalez on 12/13/17.
+//  Copyright © 2017 Andres Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
